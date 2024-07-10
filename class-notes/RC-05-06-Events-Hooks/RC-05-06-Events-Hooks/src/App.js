cfd84442af6987c30833ace71b50a669cb4c92ac
Ayca96 <aycakubra1@gmail.com>
2024-07-10 13:04:14 +0200

    "Added new components to App.js and commented out existing ones"

--- class-notes/RC-05-06-Events-Hooks/RC-05-06-Events-Hooks/src/App.js
+++ class-notes/RC-05-06-Events-Hooks/RC-05-06-Events-Hooks/src/App.js
@@ -2,15 +2,23 @@
 import Events from './1-events-hooksintro/Events';
 import Hooks from './2-hooks/Hooks';
 import Counter from './3-classComponents/Counter';
+import Form from './4-form/Form';
+import FormObject from './4-form/FormObject';
+import MouseEvents from "./5-mouse-events/MouseEvents";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Keyboard from './6-keyboard-events/Keyboard';
 
 function App() {
   return (
     <div className='App'>
     {/* <Events/> */}
     {/* <Hooks/> */}
-    <Counter/>
+    {/* <Counter/> */}
+    {/* <Form/> */}
+    {/* <FormObject/> */}
+    {/* <MouseEvents/> */}
+    <Keyboard/>
     </div>
   );
 }
