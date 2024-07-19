b4383f13c65da8e368ff3d243fae396a96788dbb
Ayca96 <aycakubra1@gmail.com>
2024-07-19 12:33:07 +0200

    commit

---
+++ class-notes/RC-11-useEffect-database-axios/src/components/BilgiList.jsx
@@ -0,0 +1,67 @@
+
+import { useState } from "react";
+import EditBilgi from "./EditBilgi";
+import { AiFillDelete } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
+
+const BilgiList = ({tutorials,deleteBilgi,putBilgiler}) => {
+  // const deleteBilgi = ()=>{
+   // }  fonksiyon sanki burda gibi halbuki home sayfasinda, ordan yolladik url orda oldugu icin.
+//console.log(tutorials);
+ const[editItem,setEditItem]=useState("");
+  
+
+  return (
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials.map(({id,title,description}) => {
+            return (
+              <tr >
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center ">
+                  <AiFillDelete
+                    type="button"
+                    size={22}
+                    className="text-danger cursor-pointer"
+                    onClick={()=> deleteBilgi(id)}
+                 
+
+                  />
+
+                  <FaEdit
+                    data-bs-toggle="modal"
+                    data-bs-target="#editModal"
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning cursor-pointer"
+                    onClick={()=> setEditItem({id,title,description})}
+                
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <EditBilgi editItem={editItem} setEditItem={setEditItem} putBilgiler={putBilgiler}/>
+    </div>
+  );
+};
+
+export default BilgiList;
+
+
+
