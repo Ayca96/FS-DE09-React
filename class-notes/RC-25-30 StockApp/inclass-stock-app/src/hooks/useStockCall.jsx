d259aba53be3ab35d6d174a6ee797e06b6283f1c
Ayca96 <aycakubra1@gmail.com>
2024-09-09 22:32:49 +0200

    "Refactored FirmCard component, updated useAuthCall and useStockCall hooks, and modified Firms page to include modal functionality"

--- class-notes/RC-25-30 StockApp/inclass-stock-app/src/hooks/useStockCall.jsx
+++ class-notes/RC-25-30 StockApp/inclass-stock-app/src/hooks/useStockCall.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
+  fetchFail,
    fetchStart, 
   //  firmSuccess, 
    getStockSuccess } from "../features/stockSlice";
 import axios from "axios";
+import useAxios from "./useAxios";
 
 const useStockCall = () => {
   const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.token);
+  const axiosWithToken =useAxios()
 
   // const getFirms = async () => {
   //   dispatch(fetchStart());
@@ -45,22 +48,78 @@ const useStockCall = () => {
     dispatch(fetchStart());
     try {
       //burdaki data axiostan gelen data
-      const { data } = await axios(`${import.meta.env.VITE_BASE_URL}${endpoint}`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      });
+      // const { data } = await axios(`${import.meta.env.VITE_BASE_URL}${endpoint}`, {
+      //   headers: {
+      //     Authorization: `Token ${token}`,
+      //   },
+      // });
+      const {data}=await axiosWithToken.get(endpoint)
       console.log(data);
       dispatch(getStockSuccess({stock: data.data, endpoint}));
     } catch (error) {
       console.log(error);
     }
   };
+
+  const deleteStockData = async(endpoint,id) =>{
+    dispatch(fetchStart());
+    try {
+    //   await axios.delete(`${import.meta.env.VITE_BASE_URL}${endpoint}/${id}`,
+
+    //  {  
+    //    headers: {
+    //       Authorization: `Token ${token}`,
+    //     },
+    //   })
+      // getStockData(endpoint)
+      await axiosWithToken.delete(`${endpoint}/${id}`)
+
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail())
+      
+    } finally{
+      getStockData(endpoint)
+    }
+  }
+
+  const postStockData = async (endpoint,info) => {
+    dispatch(fetchStart());
+    try {
+      const {data}=await axiosWithToken.post(endpoint,info)
+     
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail())
+    } finally {
+      getStockData(endpoint)
+    }
+  };
+
+  const putStockData = async (endpoint,info) => {
+    dispatch(fetchStart());
+    try {
+      const {data}=await axiosWithToken.put(`${endpoint}/${info_id}`,info)
+     
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail())
+    } finally {
+      getStockData(endpoint)
+    }
+  };
+
+
   return { 
     // getFirms, 
     // getBrands,
-    getStockData };
+    getStockData,
+    deleteStockData,
+    postStockData,
+    putStockData,
+   };
 };
+
 
 export default useStockCall;
 
